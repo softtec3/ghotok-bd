dbeb40da4d90cbbe5a672b6f0b6897de94af6f10
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2025-09-13 11:47:58 +0600

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- FontAwesome cdn -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- Google Fonts -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <title>Matrimony Website</title>
+</head>
+<body>
+    <div id="container">
+        <!-- Advertise Container 01 -->
+        <div id="advertise1">
+          
+        </div>
+        <!-- Main -->
+        <div id="main">
+            <!-- Top Bar -->
+             <div id="topBar">
+                <div><span><i class="fa-regular fa-envelope"></i></span><a href="mailto:matrimony@gmail.com">matrimony@gmail.com</a></div>
+                <div><span><i class="fa-solid fa-phone"></i></span><a href="tel:+8801318195591">+8801318195591</a></div>
+             </div>
+             <!-- Top Main Bar -->
+              <div id="topMainBar">
+                <div class="tmbLeft">
+                    <h1>Matrimony</h1>
+                </div>
+                <div class="tmbMiddle">
+                    9:30 AM
+                </div>
+                <div class="tmbRight">
+                    <span><i class="fa-regular fa-bell"></i></span>
+                    <span><i class="fa-solid fa-right-from-bracket"></i></span>
+                </div>
+              </div>
+        </div>
+        <!-- Advertise Container 02 -->
+        <div id="advertise2">
+            
+        </div>
+    </div>
+</body>
+</html>
